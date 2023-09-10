c181463c2bc813e4fad3a9e5ee2e6105ceeba360
Nirovitsky <toymammedov01@gmail.com>
2023-09-10 19:07:05 +0800

    first commit

---
+++ comments/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+from posts.models import Post
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post}"
