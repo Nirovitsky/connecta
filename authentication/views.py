97d451407e7871a06a007840f780f6b2ccb0470e
Nirovitsky <toymammedov01@gmail.com>
2024-01-27 22:50:35 +0800

    redirecting issues

--- authentication/views.py
+++ authentication/views.py
@@ -13,13 +13,13 @@ from userprofile.models import UserProfile
 class CustomLoginView(LoginView):
     template_name = 'authentication/login.html'
     redirect_authenticated_user = True
-    success_url = reverse_lazy('posts')
+    success_url = reverse_lazy('base')
 
 class RegisterPage(FormView):
     template_name = 'authentication/register.html'
     form_class = CustomUserCreationForm
     redirect_authenticated_user = True
-    success_url = reverse_lazy('posts')
+    success_url = reverse_lazy('base')
 
     def form_valid(self, form):
         if form.errors:
@@ -36,13 +36,13 @@ class RegisterPage(FormView):
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect("posts")
+            return redirect("base")
 
         return super().dispatch(request, *args, **kwargs)
 
 
 class BaseView(LoginRequiredMixin, TemplateView):
-    template_name = 'post_list.html'
+    template_name = 'base.html'
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return super().get(request, *args, **kwargs)
