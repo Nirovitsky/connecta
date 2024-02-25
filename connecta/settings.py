afb6d4f893ebbdca395c36d6c57a7d414d7a1e93
Nirovitsky <toymammedov01@gmail.com>
2024-02-26 00:09:56 +0800

    remove StaticFileFinders

--- connecta/settings.py
+++ connecta/settings.py
@@ -127,7 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 
 if not DEBUG:
